65c27936d1c12302d107a89d120d99c36835d359
BugHunter-X <askzenai@gmail.com>
2025-01-23 00:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// This code uses Tailwind classes that don't exist or are misspelled
+<div class="bg-red-500 p-4 text-white">
+  <p class="text-3xl font-bold">This is a heading</p>
+  <p class="text-xl">This is a paragraph</p>
+</div>
+```
